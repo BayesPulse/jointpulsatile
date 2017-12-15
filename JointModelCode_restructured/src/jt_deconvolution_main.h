86c54d8e07e8b917df7069ee2de85dd0850aad53
Nichole Carlson <nichole.carlson@ucdenver.edu>
2017-12-14 22:32:36 -0700

    Finished preliminary data structure for the main joint model

--- JointModelCode_restructured/src/jt_deconvolution_main.h
+++ JointModelCode_restructured/src/jt_deconvolution_main.h
@@ -6,6 +6,7 @@
 #include <time.h>
 #include <string.h>
 
+
 /*****This structure contains the information of each pulse********/
 typedef struct node_tag{
 	struct node_tag *succ;
@@ -13,17 +14,16 @@ typedef struct node_tag{
 	double time;  /*tau_ki*/
 	double mass;  /*theta^s_ki*/
     double width; /*theta^w_ki*/
-    double kappamass;
-    double kappawidth;
+    double tvarscalemass;  /*variance scale for mass t-dist*/
+    double tvarscalewidth;   /*variance scal for width t-dist*/
 	double *mean_contrib;
 	double lambda; /*for fsh pulse only, lambda, denomsum
 	                */  /***NOT SURE WHAT THIS TERM IS FOR***/
 } PulseEstimate;
 
 /****This structure contains the patient level parameters***/
-typedef struct subject_tag {
-    struct subject_tag *succ;
-    struct subject_tag *pred;
+typedef struct {
+    
     Node_type *pulselist;
     double baseline;
     double halflife;
@@ -32,19 +32,49 @@ typedef struct subject_tag {
     double logerrorsq; // log of model error (may not be used)
     double mass_mean;
     double width_mean;
-    double mass_sd;
-    double width_sd;
     int pulse_count;
     
-    /*Question: should we have the data in this structure or just the parameters*/
-    /*Question: if no data should we also move the output files to the data structure*/
-
-    char *common; /*patient level output for baseline, half-life, mass_mean, width_mean*/
-    char *pulse; /*patient level output for pulse level chains, mass, width, locations */
-    FILE *csub;
-    FILE *psub;
 } PatientEstimates;
 
+typedef struct {
+    
+    double *concentration;
+    double *time;
+    double *response_concentration;
+    double *response_time;
+    int number_of_obs;
+    double avg_period_of_obs;  /**in minutes**/
+    double duration_of_obs = number_of_obs * period_of_obs;  /**in minutes**/
+    
+    char *common; /*patient level filename for output for baseline, half-life, mass_mean, width_mean*/
+    FILE *csub;   /*patient level file for baseline, half-life, mass_mean, width_mean*/
+    char *pulse; /*patient level filename for output for pulse level chains, mass, width, locations */
+    FILE *psub; /*patient level file for above*/
+    char *resp_common; /*response hormone filename for output for baseline, half-life, mass_mean, width_mean*/
+    FILE *resp_csub;  /*response hormone patient level file for output for baseline, half-life, mass_mean, etc*/
+    char *resp_pulse; /* resp hormone filename for pulse level output: mass, width, locations*/
+    FILE *resp_psub; /*resp hormone file for pulse level output*/
+
+} PatientData;
+
+/****This structure contains the priors on the patient level info: population parameters***/
+typedef struct{
+    double cluster_size; /*cluster size (rho) in the cox process for the response hormone intensity*/
+    double log_cluster_size; /*log scale cluster size*/
+    double cluster_width; /*cluster width (nu) in the cox process for the respose hormone intensity*/
+    double log_cluster_width;
+} AssocEstimates;
+
+/****Thus structure contains the patient level data and parameters for both trigger and response hormones***/
+typedef struct patient_tag {
+    struct patient_tag *succ;
+    struct patient_tag *pred;
+    PatientEstimates *patient_estimates;
+    PatientEstimates *resp_patient_estimates;
+    PatientData *patient_data;
+    AssocEstimates *assoc_est;
+    
+};
 /*******the prior distribution is collasping all information of fsh and lh*******/
 
 typedef struct{
@@ -57,62 +87,52 @@ typedef struct{
 	double mass_mean;     /*population mean mass: mean of the patient level means*/
     double width_mean;    /*population mean width: mean of the patient level means*/
 
-	double **fe_precision; /*the variance matrix of correlated pulse mass on the population level */
+    double mass_SD;  /* population level pulse-to-pulse variation in pulse mass*/
+    double width_SD; /* population level pulse-to-pulse variation in pulse width*/
 
-	double **re_var; /*inverse of fe_precision*/
+    double mass_mean_SD; /*patient-to-patient SD in mean pulse mass*/
+    double mass_mean_variance;
 
-	double fe_precision_wf;/*sigma_w，drawe_fe_priors2*/
-	double fe_precision_wl;/*sigma_w，drawe_fe_priors2*/
+    double width_mean_SD; /*patient-to-patient SD in mean pulse width*/
+    
+} PopulationEstimates;
 
-	double *re_var_f; /*specified, the half-cauchy or uniform prior for subject-level variance */
-	double *re_var_l;
-	double alpha_f;  /* prior parameters for model error,fsh */
-	double beta_f;
-	double alpha_l;  /* prior parameters for model error,lh */
-	double beta_l;
-    double rho_prior[2]; /*log scale*/
-	double nu_prior[2];  /*log scale*/
-
-} Priors;
-
-/*the hyper is also defined together for both the driver and response */
+/*The user defined values for the priors on the population level parameters*/
+/*This structure contains all the variables that the user sets when setting the priors*/
 typedef struct {
-	double hmean_f[2]; /*ma,mw*/
-	double hmean_l[2]; /*ma,mw*/
+    double baseline_mean;
+    double baseline_variance;
+    double baseline_SD_max;
+    
+    double halflife_mean;
+    double halflife_variance;
+    double halflife_SD_max;
+    
+    double mass_mean;
+    double mass_variance;
+    double mass_SD_max;
+    
+    double width_mean;
+    double width_variance;
+    double width_SD_max;
+    
+    double alpha;  /* prior parameter in gamma for model error */
+    double beta;
+ 
+    double Range_inter1; /*(hard core): range of interaction*/
+    double Range_inter2; /*strauss range of interactio*/
+    double StraussRate; /*intensity function for strauss process: beta*/
+    double StraussRepulsion; /*repulsion function for struass: gamma */
 
-	double hvar_f; /*vw*/
-	double hvar_l; /*vw*/
-	
-	double precision_wf;
-	double precision_wl;
-	double **sig_a_inv;
+} PopulationPriors;
 
-	double **prec;  /* prior variance-covariance of pulse mass,,F-1*/
-	double meanmeanbh_l[2];  /*mhn,prior mean fo baseline and half life*/
-	double meanvarbh_l[2]; /*prior variance for baseline and halflife */
-	double varbh_l[2]; /*prior for variance for baseline and halflife*/
-	double meanmeanbh_f[2];  /*mhn,prior mean fo baseline and half life*/
-	double meanvarbh_f[2]; /*prior variance for baseline and halflife */
-	double varbh_f[2]; /*prior for variance for baseline and halflife*/
-
-} Hyper_priors;
-
-
-/**INCORRECT---INCOMPLETE EDITING
 /****This structure contains the priors on the patient level info: population parameters***/
 typedef struct{
-    
-    double Rinter1; /*(hard core)*/
-    double Rinter2; /*struass*/
-    double *re_precision;  /*nu_a,nu_w,draw_fe_precision same for each subject*/
-    int numsub;
-    int subindex;
-    int iter;
-    /*	double nprior;*/
-    double rho; /*cluster size, used for lh only,on the log scale*/
-    double lrho;
-    double lnu;
-    double nu; /*cluster width, used for lh only,it is the square not standard deviation*/
-    double beta; /*intensity function for strauss process*/
-    double gamma; /*repulsion function for struass */
-} PopulationEstimates;
+    double mean_log_cluster_size; /*mean of prior on cluster size (rho) in the cox process for the response hormone intensity*/
+    double variance_log_cluster_size; /*variance in prior on the cluster size*/
+    double mean_log_cluster_width; /*mean on prior of cluster width (nu) in the cox process for the respose hormone intensity*/
+    double variance_log_cluster_width; /*variance in prior on the cluster width*/
+} AssocPriors;
+
+
+
